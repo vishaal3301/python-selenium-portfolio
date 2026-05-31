6ef2cfd97a0c33d7d3e1f3954d4842ce71977963
Vishal Khairnar <vishalkhairnar700@gmail.com>
2026-05-31 11:08:38 +0530

    modified list_demo5

--- PythonProgrammingBasic/Day4/list_demo5.py
+++ PythonProgrammingBasic/Day4/list_demo5.py
@@ -1,2 +1,11 @@
 #To add elements/items in list
 # append() and insert()
+
+my_list = ["apple"]
+print("Original List: ",my_list)
+
+#appned() will add element at the end of the list
+my_list.append("banana")
+print("banana element is added: ",my_list)
+my_list.append("kiwi")
+print("kiwi element is added: ",my_list)
