69626a65293c94c38afeb78f7aeb89ab8c23ce7c
Vishal Khairnar <104075030+vishaal3301@users.noreply.github.com>
2026-06-07 01:03:19 +0530

    Set files add

--- PythonProgrammingBasic/Day5/set_demo2.py
+++ PythonProgrammingBasic/Day5/set_demo2.py
@@ -3,7 +3,19 @@
 # add() -> add single element at a time.
 # update() -> it will insert multiple elements at a time.
 
+# add()
 mySet2 = {'apple','banana','cherry'}
 print("Original mySet2: ",mySet2)
 mySet2.add("orange")
 print("after adding orange to mySet2: ",mySet2)
+
+# update()
+mySet3 = {'apple','banana','cherry'}
+print("original mySet3: ",mySet3)
+mySet3.update(["mango","grapes","kiwi"])
+print("after update(), mySet3 : ",mySet3)
+
+#Find Total Number of items in a Set
+# len()
+
+print("total number of items in mySet3:\t", len(mySet3))
---
+++ PythonProgrammingBasic/Day5/set_demo3.py
@@ -0,0 +1,10 @@
+"""Set is Mutable is, Hence we can Add,Remove,Update, Value inside Set"""
+# Remove elements from the Set.
+# remove(), discard()
+
+# remove()
+mySet4 = {'apple','banana','cherry','orange','kiwi'}
+print("Original mySet4:\t",mySet4)
+mySet4.remove('cherry')
+print("After removing cherry, mySet4:\t",mySet4)
+
