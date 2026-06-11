839f93e475177a9765bc751070e55efd83d47655
Vishal Khairnar <104075030+vishaal3301@users.noreply.github.com>
2026-06-11 23:06:02 +0530

    Function argument add

--- PythonProgrammingBasic/Day6/arguments_demo2.py
+++ PythonProgrammingBasic/Day6/arguments_demo2.py
@@ -1,5 +1,7 @@
 # Default values to Positional Arguments
 def add(i,j=10):
-     print(f"add(i,10) -> add({i},10) = {i+j}")
+     print(f"add(i = {i},j = {j}) -> add({i},{j}) = {i+j}")
 
 add(20)
+add(200,300)
+add(j=1.5,i=2.5)
---
+++ PythonProgrammingBasic/Day6/arguments_demo3.py
@@ -0,0 +1,7 @@
+# Keyword Arguments:
+def greetings(name,greet_msg):
+        print(f"{greet_msg}\t{name} ")
+
+greetings("vishaal","Welcome")
+
+greetings(greet_msg="Welcome",name="Vishaal")
