763285b523205e94d16218f63acbcccbd305af98
Vishal Khairnar <104075030+vishaal3301@users.noreply.github.com>
2026-06-12 00:01:27 +0530

    Argumets concept complete

--- PythonProgrammingBasic/Day6/arguments_demo5.py
+++ PythonProgrammingBasic/Day6/arguments_demo5.py
@@ -1,1 +1,15 @@
-def
+# Return two values
+# Function can return the multiple values.
+def largest(a,b):
+    if a>b:
+        return a,b
+    else:
+        return b,a
+
+print(f"largest Value is : {largest(100,200)}")
+print(f"largest Value is : {largest(300,200)}")
+
+
+result = largest(786,586)
+print(f"{result}")
+print(type(result))
