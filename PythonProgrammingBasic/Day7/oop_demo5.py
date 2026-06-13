42bad38337a5ae45d06d4a0cddcbd9a99fdd9b12
Vishal Khairnar <104075030+vishaal3301@users.noreply.github.com>
2026-06-14 00:06:52 +0530

    oop_demo5 file added

--- PythonProgrammingBasic/Day7/oop_demo5.py
+++ PythonProgrammingBasic/Day7/oop_demo5.py
@@ -5,12 +5,12 @@ class m2:
     def m2_print(self):
         print(f"{self.a,self.b}") #Class variables are access using self keyword.
 
-    def m2_additon(self):
+    def m2_addition(self):
         print(f"a\t+\tb\t=\t{self.a+self.b}")
     def m2_multiply(self):
         print(f"a\t*\tb\t=\t{self.a*self.b}")
 
 obj_m2 = m2()
 obj_m2.m2_print()
-obj_m2.m2_additon()
+obj_m2.m2_addition()
 obj_m2.m2_multiply()
